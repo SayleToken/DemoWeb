abc8875bfa9dd525d2fb4f2acb2c56b9fad7d892
Kassem <kassemshehady@gmail.com>
2018-06-08 07:26:41 +0300

    Update transactions.service.ts

--- src/app/main/content/transaction/transactions.service.ts
+++ src/app/main/content/transaction/transactions.service.ts
@@ -66,7 +66,7 @@ export class TransactionsService implements Resolve<any>
     getContacts(): Promise<any>
     {
         return new Promise((resolve, reject) => {
-                this.http.get('http://ec2-52-17-126-108.eu-west-1.compute.amazonaws.com:3000/api/transactions')
+                this.http.get('http://ec2-34-245-47-221.eu-west-1.compute.amazonaws.com:3000/api/transactions')
                     .subscribe((response: any) => {
 
                         this.contacts = response.data.docs;
@@ -161,7 +161,7 @@ export class TransactionsService implements Resolve<any>
     {
         return new Promise((resolve, reject) => {
 
-            this.http.post('http://ec2-52-17-126-108.eu-west-1.compute.amazonaws.com:3000/api/transactions/', {...contact})
+            this.http.post('http://ec2-34-245-47-221.eu-west-1.compute.amazonaws.com:3000/api/transactions/', {...contact})
                 .subscribe(response => {
                     this.getContacts();
                     resolve(response);
@@ -173,7 +173,7 @@ export class TransactionsService implements Resolve<any>
     {
         return new Promise((resolve, reject) => {
 
-            this.http.put('http://ec2-52-17-126-108.eu-west-1.compute.amazonaws.com:3000/api/transactions/', {...contact})
+            this.http.put('http://ec2-34-245-47-221.eu-west-1.compute.amazonaws.com:3000/api/transactions/', {...contact})
                 .subscribe(response => {
                     this.getContacts();
                     resolve(response);
